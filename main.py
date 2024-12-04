c981f2608fb38229be92d34c497334b30e71902f
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-12-04 19:53:29 -0300

    refactor to clear superfluous messages

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    numpass, numfail = pygame.init()
-    print(f"PyGame init: {numpass=}, {numfail=}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
