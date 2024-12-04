d730dcb9c6d8a59484212987c225d8ef31eec55c
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-12-04 19:51:00 -0300

    add window initialization

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    numpass, numfail = pygame.init()
+    print(f"PyGame init: {numpass=}, {numfail=}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(pygame.Color("black"))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
